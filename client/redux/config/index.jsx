14157506c1824088ff37cf48c40e1e0e2b6ca2b6
luisdanielmv <luisdanielmv@gmail.com>
2017-06-26 12:37:29 -0600

    Client register functional (no feedback)

--- client/redux/config/index.jsx
+++ client/redux/config/index.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export function getAxiosInstance() {
-    const baseURL = 'https://powerful-caverns-20187.herokuapp.com/api',
+    const baseURL = 'https://bstrello.herokuapp.com/api',
           headers = { Authorization: localStorage.getItem('jwt') }
 
     const instance = axios.create({baseURL, headers});
