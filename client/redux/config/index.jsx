2ff2c40683177b3a23658a959b73b20b9dd63910
luisdanielmv <luisdanielmv@gmail.com>
2017-06-25 23:51:44 -0600

    HEROKU Deployment - fixing issues

--- client/redux/config/index.jsx
+++ client/redux/config/index.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export function getAxiosInstance() {
-    const baseURL = 'http://localhost:7777/api',
+    const baseURL = 'https://bstrello.herokuapp.com/api',
           headers = { Authorization: localStorage.getItem('jwt') }
 
     const instance = axios.create({baseURL, headers});
